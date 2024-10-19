f64a0e4d2fbeac150c434e9639a28bb3227c8aed
Edmon Begoli <ebegoli@gmail.com>
2024-10-19 15:02:44 -0400

    Update gmm.h
    Support for incremental updates.

--- src/gmm.h
+++ src/gmm.h
@@ -1,16 +1,18 @@
 #ifndef GMM_H
 #define GMM_H
 
-#define MAX_CLUSTERS 5  // Max number of GMM clusters
+#define MAX_CLUSTERS 5  // Max number of clusters
 
 typedef struct {
-    double means[MAX_CLUSTERS][2];      // Mean vectors (2D example)
+    double means[MAX_CLUSTERS][2];      // Mean vectors (2D case)
     double covariances[MAX_CLUSTERS][2][2];  // Covariance matrices
     double weights[MAX_CLUSTERS];       // Mixture weights
+    int cluster_counts[MAX_CLUSTERS];   // Number of points assigned to each cluster
     int num_clusters;                   // Number of clusters
 } GMM;
 
 void fit_gmm(GMM *model, double data[][2], int n, int k);
 void print_gmm(const GMM *model);
+void update_gmm(GMM *model, double new_point[2]);  // For incremental updates
 
 #endif  // GMM_H
