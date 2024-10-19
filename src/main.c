9095c064410269fad040d644a92d4c5c0077ee3e
Edmon Begoli <ebegoli@gmail.com>
2024-10-19 14:51:39 -0400

    Update main.c
    Added extended functionality.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,25 @@
-
 #include <stdio.h>
+#include "gmm.h"
+
+void demo_tiny_reducer_db() {
+    // Sample data (2D points)
+    double data[6][2] = { {1.0, 2.0}, {2.0, 3.0}, {3.0, 4.0},
+                          {8.0, 7.0}, {9.0, 6.0}, {10.0, 8.0} };
+    GMM model;
+
+    // Fit GMM with 2 clusters
+    fit_gmm(&model, data, 6, 2);
+
+    // Save to DB
+    save_gmm_to_db(&model);
+
+    // Load from DB and print
+    GMM loaded_model;
+    load_gmm_from_db(&loaded_model);
+    print_gmm(&loaded_model);
+}
 
 int main() {
-    printf("Welcome to TinyReducerDB!\n");
-    // Initialization of the database and GMM logic goes here
+    demo_tiny_reducer_db();
     return 0;
 }
